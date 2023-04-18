f7740d70e3f3c057497d07aa67d002eac522009d
FlorianZasada <florian.zasada@gmail.com>
2023-04-18 18:19:20 +0200

    add other sections

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -101,6 +101,165 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
+.header-view-center-image {
+  color: white;
+  background-image: url("https://cdn.pixabay.com/photo/2014/09/07/22/17/forest-438432_1280.jpg");
+  /* width: 100%; */
+  height: 100vh;
+  object-fit: fill;
+  padding: .5rem;
+
+  display: flex;
+
+}
+
+.header-view-center-container {
+  width: 60%;
+  display: flex;
+  flex-direction: column;
+  margin: auto;
+  padding: 1rem;
+  border-radius: 8px;
+  align-items:center;
+  justify-content:center;
+  gap: 1rem;
+  background: rgba(0, 0, 0, 0.313);
+}
+
+.header-view-center-container h1{
+  color: rgb(241, 228, 183);
+  padding: 2rem;
+  text-align: center;
+}
+
+span {
+  font-size: medium;
+  color: white;
+}
+
+.motivation-frame {
+  display: flex;
+  background-color: black;
+  padding: 3rem;
+  justify-content: center;
+}
+
+.motivation-frame-inner {
+  margin: 3rem;
+  padding: 3rem;
+  border: 3px solid white;
+  text-align: center;
+
+  display: flex;
+  flex-direction: column;
+  gap: 2rem;
+  align-items: center;
+}
+
+.termine-frame {
+  display: flex;
+  background-image: url("https://cdn.pixabay.com/photo/2013/10/02/23/03/mountains-190055_1280.jpg");
+  padding: 3rem;
+  justify-content: center;
+}
+
+.termine-frame-inner {
+  display: flex;
+  flex-direction: column;
+  gap: 2rem;
+  align-items: center;
+  max-width: 60%;
+}
+
+footer {
+  padding: 2rem;
+  background-color: var(--primary-color);
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  gap: 1rem;
+}
+
+.kontakt-frame {
+  display: flex;
+  padding: 3rem;
+  justify-content: center;
+}
+
+.kontakt-frame-inner {
+  display: flex;
+  flex-direction: column;
+  justify-items: center;
+  align-items: center;
+}
+
+.leistungen-frame {
+  background-color: black;
+  display: flex;
+  padding: 3rem;
+  justify-content: center;
+}
+
+.leistungen-frame-inner {
+  color: white;
+  display: flex;
+  flex-direction: column;
+  justify-items: center;
+  align-items: center;
+}
+
+table th, table td{
+  padding: .8rem;
+}
+
+
+@media only screen and (max-width: 600px) {
+  /* .header-view-center-image {
+
+  } */
+
+    .header-view-center-container{
+      width: 100%
+    }
+
+    .motivation-frame {
+      padding: 1rem;
+    }
+    .motivation-frame-inner {
+      margin: 0;
+      padding: 1rem;
+      border: 1px solid white;
+
+    }
+    .termine-frame {
+      padding: 1rem;
+    }
+
+    .termine-frame-inner{
+      max-width: 100%;
+      padding: 1rem;
+    }
+    
+    .kontakt-frame-inner{
+      padding: 1rem;
+    }
+
+    .kontakt-frame{
+      padding: 1rem;
+    }
+
+    .leistungen-frame{
+      padding: 1rem;
+    }
+
+    .leistungen-frame-inner{
+      padding: 1rem;
+    }
+
+    table th, table td {
+      padding: .3rem;
+    }
+}
 
 </style>
